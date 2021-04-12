f456ad3b46db7fed5bda8f754c097d262857f190
mashrur23 <navidmash17@gmail.com>
2021-04-12 21:28:06 +0600

    changed appbar name

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("valorant")),
+    return Scaffold(appBar: AppBar(title: Text("valorant guides")),
       
     );
   }
